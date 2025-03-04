1250c5f407010a9f449a74b0c0211919cc78918f
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-04 20:51:26 +0100

    Create the content of contact-page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
 import NavBar from "@/components/ui/navbar";
+import Footer from "@/components/ui/footer";
+import ContactIcons from "@/components/ui/contact-icons";
 
 export default function Contact() {
   return (
     <>
       <NavBar />
       <MaxWidthWrapper>
-        <h1>Contact</h1>
+        <main className="h-[85vh] my-auto grid md:grid-cols-2">
+          <section className="flex flex-col justify-center">
+            <span className="block font-handwritten text-6xl lowercase animate-slide-in-left">
+              let's
+            </span>
+            <h1 className="text-7xl lg:text-9xl mb-5 font-secondary uppercase animate-slide-in-right">
+              Get in touch
+            </h1>
+          </section>
+
+          {/* TODO: update to a contact form */}
+          <section className="flex flex-col justify-center items-center gap-5 bg-foreground text-background">
+            <div className="text-center w-[fit-content] text-lg">
+              <p className="uppercase mb-0">Email</p>
+              <a
+                className="border-b-2 border-dashed border-background"
+                href="mailto:sandra.hkannerberg@gmail.com"
+              >
+                sandra.hkannerberg@gmail.com
+              </a>
+            </div>
+
+            <ContactIcons></ContactIcons>
+          </section>
+        </main>
       </MaxWidthWrapper>
+      <Footer></Footer>
     </>
   );
 }
