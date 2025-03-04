2af4253d334b416a83451898d418e9d1be5b7fd4
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-04 13:42:28 +0100

    Change font-family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>Portfolio</h1>
+    </main>
+  );
 }
