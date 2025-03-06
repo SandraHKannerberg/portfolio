2e917aeb7d42062b3ad5dfcb91bde1b233edfff8
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-06 15:09:30 +0100

    Optimizing animation for small devices, no the animation both have scroll and touch trigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,34 @@ export default function HomePage() {
   // Smooth scrolling to AboutPage
   const router = useRouter();
   const [scrolling, setScrolling] = useState(false);
+  const [touchStartY, setTouchStartY] = useState(0);
 
   useEffect(() => {
     router.prefetch("/about"); // Preload AboutPage
   }, [router]);
 
+  // Handle scroll on desktop-device
   const handleScroll = () => {
     if (!scrolling) {
       setScrolling(true);
       setTimeout(() => {
         router.push("/about");
       }, 500);
+    }
+  };
+
+  // Handle touch on mobile-device
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setTouchStartY(e.touches[0].clientY);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    const touchEndY = e.touches[0].clientY;
+    const deltaY = touchStartY - touchEndY;
+
+    if (deltaY > 50) {
+      // Swipe up
+      handleScroll();
     }
   };
 
@@ -33,7 +50,9 @@ export default function HomePage() {
         scrolling ? { scale: 0.5, opacity: 0 } : { scale: 1, opacity: 1 }
       }
       transition={{ duration: 0.7 }}
-      onWheel={handleScroll}
+      onWheel={handleScroll} // Desktop
+      onTouchStart={handleTouchStart} // Mobile
+      onTouchMove={handleTouchMove} // Mobile
     >
       {/* Hero section */}
       <header className="flex flex-col items-center justify-center relative w-screen h-screen">
