917fa1ff608bc36185bb9ef33214ccf2499bb3de
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-06 15:08:07 +0100

    Add cv as pdf to be able to download it

--- src/components/work/cv-btn.tsx
+++ src/components/work/cv-btn.tsx
@@ -3,18 +3,18 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 export default function CvBtn() {
-  // const handleDownload = () => {
-  //   const link = document.createElement("a");
-  //   link.href = "/files/sandra-kannerberg-cv.pdf";
-  //   link.download = "sandra-kannerberg-cv.pdf"; // Custome file-name
-  //   link.click();
-  // };
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/files/sandra-kannerberg-cv.pdf";
+    link.download = "sandra-kannerberg-cv.pdf"; // Custome file-name
+    link.click();
+  };
 
   return (
     <Button
       variant="default"
-      className="rounded uppercase cursor-pointer shadow mb-5 hover:bg-secondary hover:text-background hover:shadow-md animate-bounce"
-      // onClick={handleDownload}
+      className="rounded uppercase cursor-pointer shadow mt-5 mb-10 hover:bg-secondary hover:text-background hover:shadow-md animate-bounce"
+      onClick={handleDownload}
     >
       Download CV
     </Button>
