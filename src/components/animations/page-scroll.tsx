71f923cd72591426ac78869f8fe44c6a6d63bf50
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-20 16:22:57 +0100

    Update animation page-scroll to also be triggered by button-click

--- src/components/animations/page-scroll.tsx
+++ src/components/animations/page-scroll.tsx
@@ -2,6 +2,8 @@
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import { ReactNode, useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { ArrowDown } from "lucide-react";
 
 interface PageScrollProps {
   children: ReactNode;
@@ -46,18 +48,34 @@ export default function PageScroll({
     }
   };
 
+  // Handle button click if scroll not possible
+  const handleButtonClick = () => {
+    setScrolling(true);
+    setTimeout(() => {
+      router.push("/about");
+    }, 500);
+  };
+
   return (
-    <motion.div
-      className={`relative w-full min-h-[300px] will-change-transform ${className}`}
-      animate={
-        scrolling ? { scale: 0.5, opacity: 0 } : { scale: 1, opacity: 1 }
-      }
-      transition={{ duration: 0.7, ease: "easeInOut" }}
-      onWheel={handleScroll} // Desktop
-      onTouchStart={handleTouchStart} // Mobile
-      onTouchMove={handleTouchMove} // Mobile
-    >
-      {children}
-    </motion.div>
+    <>
+      <motion.div
+        className={`relative w-full min-h-[300px] will-change-transform ${className}`}
+        animate={
+          scrolling ? { scale: 0.5, opacity: 0 } : { scale: 1, opacity: 1 }
+        }
+        transition={{ duration: 0.7, ease: "easeInOut" }}
+        onWheel={handleScroll} // Desktop
+        onTouchStart={handleTouchStart} // Mobile
+        onTouchMove={handleTouchMove} // Mobile
+      >
+        {children}
+      </motion.div>
+      <Button
+        onClick={handleButtonClick}
+        className="absolute bottom-23 left-1/2 transform -translate-x-1/2 px-4 py-2 h-12 w-12 flex justify-center items-center rounded-full shadow-md animate-bounce bg-muted hover:bg-muted hover:border hover:border-2-foreground cursor-pointer"
+      >
+        <ArrowDown />
+      </Button>
+    </>
   );
 }
