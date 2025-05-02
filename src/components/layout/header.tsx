bc57b4905ef068db52cd205adafb0aa48034b5ee
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-05-02 14:31:33 +0200

    fix: renamed component header-with-navbar to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,15 @@
 "use client";
-import NavBar from "./navbar";
-import FullscreenMenu from "./fullscreen-menu";
+import NavBar from "../navigation/navbar";
+import FullscreenMenu from "../navigation/fullscreen-menu";
 import { useIsMobile } from "@/hooks/use-mobile";
 
-export default function HeaderWithNavBar() {
+const Header = () => {
   const isMobile = useIsMobile();
-
   return (
     <header className="p-3 sticky top-0 bg-background/75 z-[100] backdrop-blur-lg transition-all shadow-sm mb-5">
       {isMobile ? <FullscreenMenu></FullscreenMenu> : <NavBar></NavBar>}
     </header>
   );
-}
+};
+
+export default Header;
