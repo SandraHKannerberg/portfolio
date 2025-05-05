99ee7b3a037529bae0482f8ae0af03fd49e39f05
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-05-05 14:07:51 +0200

    feat: add my name in the header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,12 +2,17 @@
 import NavBar from "../navigation/navbar";
 import FullscreenMenu from "../navigation/fullscreen-menu";
 import { useIsMobile } from "@/hooks/use-mobile";
+import Link from "next/link";
 
 const Header = () => {
   const isMobile = useIsMobile();
   return (
-    <header className="p-3 sticky top-0 bg-background/75 z-[100] backdrop-blur-lg transition-all shadow-sm mb-5">
-      {isMobile ? <FullscreenMenu></FullscreenMenu> : <NavBar></NavBar>}
+    <header className="sticky top-0 px-5 bg-background/75 z-[100] backdrop-blur-lg transition-all shadow-sm flex justify-between items-center m-auto">
+      <Link href="/">
+        <p className="font-secondary text-5xl">sandra.</p>
+      </Link>
+
+      {isMobile ? <FullscreenMenu /> : <NavBar />}
     </header>
   );
 };
