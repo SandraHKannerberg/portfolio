d180e876dd08d4dc7b9151c238c8f7db3a90d242
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-05-07 12:01:14 +0200

    feat: import component ContactLinks

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { contactLinks } from "@/constants";
-import LinkWithIcon from "../ui/link-with-icon";
-import { ArrowUpRight } from "lucide-react";
+import ContactLinks from "../contact/contact-links";
 
 const Footer = () => {
   return (
@@ -20,17 +18,7 @@ const Footer = () => {
             </Button>
           </Link>
         </div>
-        <section className="flex flex-col gap-3">
-          {contactLinks.map((link, index) => (
-            <LinkWithIcon
-              key={index}
-              label={link.label}
-              url={link.url}
-              className="justify-between"
-              newTab
-            />
-          ))}
-        </section>
+        <ContactLinks />
       </section>
 
       <small className="text-center text-xs mt-5">
