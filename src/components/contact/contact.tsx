9b863e7dad42802e9008c964cfac27d7e018948b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-05-16 10:05:38 +0200

    feat: create a new function parallaxScroll. Use it in contact-section for parallax scroll effect on contact-form.

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -2,24 +2,51 @@ import React from "react";
 import ContactForm from "@/components/contact/contact-form";
 import TextAnimWrapper from "../animations/text-anim-wrapper";
 import ContactBtn from "./contact-btn";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { parallaxScroll } from "@/lib/utils/animations";
 
 const Contact = () => {
+  const parallaxContainerRef = useRef(null);
+  const parallaxItemRef = useRef(null);
+  const textRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    if (
+      !parallaxItemRef.current ||
+      !parallaxContainerRef.current ||
+      !textRef.current
+    )
+      return;
+    parallaxScroll(
+      parallaxItemRef.current,
+      textRef.current,
+      parallaxContainerRef.current
+    );
+  }, []);
   return (
-    <section id="contact" className="grid md:grid-cols-2 gap-[10rem]">
-      <section className="flex flex-col gap-3 justify-center items-center md:items-start md:text-left col-span-2">
+    <section
+      ref={parallaxContainerRef}
+      id="contact"
+      className="grid md:grid-cols-2 gap-[10rem] relativ min-h-screen"
+    >
+      <section className="flex flex-col gap-3 justify-center items-center md:items-start md:text-left col-span-2 sticky top-32 z-10">
         <TextAnimWrapper>
           <div className="flex flex-col mx-auto">
             <p className="block font-handwritten text-6xl lg:text-[8rem] lowercase px-5">
               let&apos;s
             </p>
-            <h2 className="text-7xl lg:text-[10rem] mb-5 font-secondary uppercase">
+            <h2
+              className="text-7xl lg:text-[10rem] mb-5 font-secondary uppercase"
+              ref={textRef}
+            >
               Get in touch
             </h2>
           </div>
         </TextAnimWrapper>
 
         <TextAnimWrapper>
-          <p className="indent-8">
+          <p className="indent-10">
             Ready to built amazing things together? Reaching out to offer a new
             opportunity? Just want to say Hello? Feel free to fill out the form
             or send me an e-mail—let's connect today!
@@ -30,8 +57,8 @@ const Contact = () => {
       </section>
 
       <section
-        className="col-span-2 flex gap-3 justify-end items-end bg-foreground text-background shadow-lg rounded py-5 my-10 md:my-0 w-full md:w-[50vw] lg:w-[25vw]  justify-self-end"
-        data-speed=".4"
+        ref={parallaxItemRef}
+        className="col-span-2 flex gap-3 justify-end items-end bg-foreground text-background shadow-lg rounded py-5  my-10 md:my-0 w-full md:w-[50vw] lg:w-[25vw] justify-self-end relative z-20"
       >
         <ContactForm />
       </section>
